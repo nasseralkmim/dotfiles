8b34e965029c3ceacca9549603839fea1eaeffdb
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-08-15 13:19:47 +0200

    wezterm: add

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Github'
+
+config.enable_wayland = false
+config.font = wezterm.font 'Monaspace Neon'
+config.font_size = 14
+config.enable_tab_bar = false
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
